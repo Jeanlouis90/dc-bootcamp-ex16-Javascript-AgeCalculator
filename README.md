146ba148864320b18a2d5e6782fcc4d657d77f7e
Jeanlouis90 <jeanlouisjabouin@gmail.com>
2018-03-31 21:49:53 -0400

    First commit. The Age Calculator takes the YEARLY AGE of the user and returns the value of the users age in DAYS. Try it out!

---
+++ README.md
@@ -0,0 +1,1 @@
+# dc-bootcamp-ex16-Javascript-AgeCalculator
---
+++ script.js
@@ -0,0 +1,3 @@
+var ageInYears = prompt("How old are you (IN YEARS)?");
+var ageInDays = ageInYears * 365.25;
+alert(ageInYears + " years is roughly " + ageInDays + " days.");
